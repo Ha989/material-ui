3cdd7421ed45958887a4f478533920bcf1860dba
Ha989 <viethanguyen0809@gmail.com>
2022-12-30 14:34:00 +0700

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavBar from "./components/NavBar";
+import JobCards from "./components/JobCards";
+import jobs from "./jobs.json";
+import Grid from '@mui/material/Grid';
+import { createTheme, ThemeProvider } from '@mui/material';
 import './App.css';
+import BasicPagination from './components/BasicPagination';
+
+const theme = createTheme({
+    palette: {
+          primary: {
+          main: '#482880',
+          light: 'rgba(238,244,240,0.6)',
+          dark: '#fff'
+        },
+        secondary: {
+          main: '#e3f2fd',
+          light: '#e09926'
+        },
+      },
+    })
 
 function App() {
+
+    
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <ThemeProvider theme={theme}>
+        <NavBar />
+        <div className='container'>
+        <Grid container spacing={2} sx={{ mt: 5}}>
+        {jobs.slice(0,5).map(job => (
+        <Grid key={job.id} item xs={12} md={6} lg={4}>
+          <JobCards job={job}/>
+          </Grid>
+        ))
+        }
+        </Grid>
+        <BasicPagination />
+        </div>
+        </ThemeProvider>
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/JobCards.js
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { Button } from '@mui/material';
+
+import Typography from '@mui/material/Typography';
+
+
+// const bull = (
+//   <Box
+//     component="span"
+//     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+//   >
+//     •
+//   </Box>
+// );
+export default function OutlinedCard({ job }) {
+    const card = (
+        <React.Fragment>
+          <CardContent >
+            <Typography variant="h5" component="div">
+              {job.title}
+              <hr/>
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary" >
+              {job.skills.slice(0,4).map( (skill, index) => (
+               <Button variant="outlined"key={index} size='small' sx={{ mr: 1, backgroundColor: 'secondary.main'}}>
+              {skill}
+             </Button>
+              )
+              )}            
+            </Typography>
+            <Typography variant="body2" >
+              {job.description}
+              <br />
+            </Typography>
+            <Typography variant="h6" color="text.primary.dark">
+              <br />
+              Salary: ${job.salaryHigh}
+            </Typography>
+            <Typography variant="h6" color="text.primary.dark">
+          
+              Location: {job.city}
+            </Typography>
+          </CardContent>
+          <CardActions >
+            <Button variant="outlined" size="small" sx={{backgroundColor: 'secondary.light'}}>Learn More</Button>
+          </CardActions>
+        </React.Fragment>
+      );
+    return (
+      <Box sx={{ minWidth: 275}}>
+        <Card variant="outlined" sx={{backgroundColor: 'primary.light' }}  >{card}</Card>
+      </Box>
+    );
+  }
+
+
+
